ef0f4db58f27b770ef0fee88d2b74cb5b53713a8
Danylo <danillazorenko12@gmail.com>
2020-11-10 18:35:19 +0200

    add sectiom product-list

--- dir/js/main.js
+++ dir/js/main.js
@@ -4,15 +4,19 @@ $(function(){
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>'
     });
-    $('.search__tabs-item').on('click', function(e){
+    $('.tab').on('click', function(e){
         e.preventDefault();
         
-        $('.search__tabs-item').removeClass('search__tabs-item--active');
-        $('.search__content-item').removeClass('search__content-item--active');
+        $('.tab').removeClass('tab--active');
+        $('.tabs-content').removeClass('tabs-content--active');
 
-        $(this).addClass('search__tabs-item--active');
-        $($(this).attr('href')).addClass('search__content-item--active')
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
         
+    });
+
+    $('.product-item__faivorite').on('click', function(){
+        $('.product-item__faivorite').toggleClass('product-item__faivorite--active');
     });
 
 });
